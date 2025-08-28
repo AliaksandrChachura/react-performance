f742b9d5a9911f31f58189477fcdb119fdcb5e6b
Aliaksandr Chachura <ales@Aliaksandrs-MacBook-Pro.local>
2025-08-28 13:50:31 +0200

    feat: add prettier, eslint, husky

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,38 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/test/setup.ts'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      exclude: [
+        'node_modules/',
+        'src/test/',
+        '**/*.d.ts',
+        '**/*.config.ts',
+        'dist/',
+        'src/constants/**',
+        'keys.ts',
+        'eslint.config.js',
+        'src/features/**/**',
+        'src/routes/**/**',
+        'src/hooks/**/**',
+        'src/test/**/**',
+        'src/types/**',
+      ],
+      thresholds: {
+        global: {
+          branches: 50,
+          functions: 50,
+          lines: 50,
+          statements: 80,
+        },
+      },
+    },
+  },
+});
